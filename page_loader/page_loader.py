03a903f18a5e7e5284a01a2e86a80c49f6ca4812
Pavel Soroka <40a.pvl@gmail.com>
2023-11-09 13:16:08 +0200

    Fix a bug in page_loader.get_filename_for_saving_web_content()

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,16 +1,29 @@
 import os
 import re
+import requests
 
 
 def download(url, path=None):
     path = path or os.getcwd()
-    return path
+    path_to_save_web_content = os.path.join(
+        path,
+        get_filename_for_saving_web_content(url)    
+    )
+
+    request = requests.get(url)
+    content_to_save = request.text
+
+    with open(path_to_save_web_content, 'w') as f:
+        f.write(content_to_save)
+
+    return path_to_save_web_content
 
 
 def get_filename_for_saving_web_content(url):
     if url.startswith('http'):
         _, url = url.split('//')
-
+    url = url.rstrip('/')
+    
     separators = re.compile(r'[./]')
     url_tokens = separators.split(url)
     base_filename = '-'.join(url_tokens)
