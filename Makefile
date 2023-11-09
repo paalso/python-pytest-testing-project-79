f6da4826f5fdc32f2912b28a0a4545161ee3fbd1
Pavel Soroka <40a.pvl@gmail.com>
2023-11-09 15:47:51 +0200

    Add sript page_loader/scripts/main.py

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ install:
 test:
 	poetry run pytest
 
+test-cov:
+	poetry run pytest --cov
+
 lint:
 	poetry run flake8 page_loader
 	poetry run flake8 tests
@@ -15,5 +18,8 @@ check: selfcheck test lint
 
 build: check
 	poetry build
+
+run:
+	poetry run page_loader/scripts/main.py -o tmp $(ARGS)
 
 .PHONY: install test lint selfcheck check build
---
+++ page_loader/scripts/main.py
@@ -0,0 +1,21 @@
+import argparse
+from page_loader.page_loader import download
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Download web page')
+
+    parser.add_argument('url', metavar='url')
+    parser.add_argument('-o', '--output', help='set directory of output')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    url, path = args.url, args.output
+    download(url, path)
+
+
+if __name__ ==  '__main__':
+    main()
