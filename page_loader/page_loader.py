96fceb536770f1963313ec1364969db72cb6a215
Pavel Soroka <40a.pvl@gmail.com>
2023-11-16 13:40:36 +0200

    WIP: adding logic to download images

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,101 +1,68 @@
 import os
-import re
 import requests
 from bs4 import BeautifulSoup
-import urllib.parse
+from . import url_utils
 
 
 class PageLoader:
     def __init__(self, url, path=None):
         self.url = url
-        self.netloc = self.__get_netloc()
         self.path = path or ''
         self.path_to_save_page_content = os.path.join(
-            self.path,
-            self.__get_filename_for_saving_web_content()
-        )
-        self.raw_page_content = self.__get_raw_page_content()
-        self.soup = BeautifulSoup(self.raw_page_content, 'html.parser')
-        self.prettified_page_content = self.soup.prettify()
-        print(self.url)
+            self.path, url_utils.filename_from_url(self.url))
+        self.resources_dir = url_utils.dirname_for_web_resources(
+            url_utils.filename_from_url(self.url))
+        self.resources_path = url_utils.dirname_for_web_resources(
+            self.path_to_save_page_content)
+
+        self.original_page_content = self.__get_raw_page_content()
+        self.soup = BeautifulSoup(self.original_page_content, 'html.parser')
+        self.processed_page_content = self.soup.prettify()
         print()
-        print(PageLoader.__get_filename_from_url(self.url))
-        print(PageLoader.__get_filename_from_url('assets/professions/python.png'))
-        print(PageLoader.__get_filename_from_url('https://paalso.github.io/simple_web_page.html'))
-        print()
-        print(self.__get_filename_for_saving_web_content())
 
     def download(self):
+        self.__download_resources()
+
+        processed_page_content = self.soup.prettify()
         with open(self.path_to_save_page_content, 'w') as f:
-            f.write(self.prettified_page_content)
+            f.write(processed_page_content)
 
         return self.path_to_save_page_content
+
+    def __download_resources(self):
+        os.makedirs(self.resources_path)
+        for resource in self.__get_page_resources():
+            resource_path = resource.get('src')
+            resource_full_url = url_utils.full_url(self.url, resource_path)
+            path_to_save_resource = self.__get_path_to_save_recourse(resource_path)
+            new_resource_basename = os.path.basename(path_to_save_resource)
+            new_resource_path = os.path.join(self.resources_dir, new_resource_basename)
+            self.__download_resource(resource_full_url, path_to_save_resource)
+            resource['src'] = new_resource_path
+            print()
 
     def __get_raw_page_content(self):
         request = requests.get(self.url)
         return request.text
 
-    def __get_filename_for_saving_web_content(self):
-        """
-        Generate a filename for saving the web content based on the URL.
+    def __get_page_resources(self):
+        return self.soup.find_all('img')
 
-        Example:
-            'https://paalso.github.io/simple_web_page/' ->
-            'paalso-github-io-simple_web_page.html'
-        """
-        if self.url.startswith('http'):
-            _, url = self.url.split('//')
-        url = url.rstrip('/')
+    def __get_path_to_save_recourse(self, resource_url):
+        return os.path.join(
+            self.resources_path,
+            url_utils.filename_from_url(resource_url))
 
-        separators = re.compile(r'[./]')
-        url_tokens = separators.split(url)
-        base_filename = '-'.join(url_tokens)
-
-        return f'{base_filename}.html'
-
-    def __get_dirname_for_saving_web_resources(self, content_filename):
-        """
-        Generate a directory name for saving web resources based on the content filename.
-
-        Example:
-            'paalso-github-io-simple_web_page.html' ->
-            'paalso-github-io-simple_web_page_files'
-        """
-        basename, _ = content_filename.split('.')
-        return f'{basename}_files'
-
-    @staticmethod
-    def __get_filename_from_url(url):
-        """
-        Generate a filename from a (possibly partial) url
-
-        Example:
-            'assets/img/omelette.png' ->
-            'assets-img-omelette.png'
-        """
-        parsed_url = urllib.parse.urlparse(url)
-        netloc_part = parsed_url.netloc.replace('.', '-')
-        path_part = parsed_url.path.replace('/', '-')
-        return f'{netloc_part}{path_part}'
-
-    def __get_page_resources_full_links(self):
-        links = self.__get_page_image_links()
-        full_links = [self.__get_full_resource_link(link) for link in links]
-        return full_links
-
-    def __get_page_image_links(self):
-        image_links = (img.get('src') for img in self.soup.find_all('img'))
-        image_full_links = [self.__get_full_resource_link(link)
-                            for link in image_links]
-        return image_full_links
-
-    def __get_full_resource_link(self, partial_link):
-        if urllib.parse.urlparse(partial_link).scheme:
-            return partial_link
-        return urllib.parse.urljoin(self.url, partial_link)
-
-    def __get_netloc(self):
-        return urllib.parse.urlparse(self.url).netloc
+    def __download_resource(self, url, save_path):
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            print(f"resource file '{url}' downloaded successfully"
+                  f"and saved to '{save_path}'")
+        else:
+            print(f"Failed to download image."
+                  f"Status code: {response.status_code}")
 
 
 def download(url, path=None):
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -5,25 +5,25 @@ import pytest
 
 from page_loader.page_loader import download
 
-URL = 'https://ru.hexlet.io/courses'
+URL = "https://ru.hexlet.io/courses"
 
 
 @pytest.fixture
 def expected_content():
-    path = os.path.join('tests', 'fixtures', 'expected.html')
+    path = os.path.join("tests", "fixtures", "expected.html")
     with open(path) as f:
         return f.read()
 
 
 @pytest.fixture
 def retrieved_content():
-    path = os.path.join('tests', 'fixtures', 'retrieved.html')
+    path = os.path.join("tests", "fixtures", "retrieved.html")
     with open(path) as f:
         return f.read()
 
 
 def test_download_html(expected_content, retrieved_content):
-    subdir = os.path.join('some_dir', 'subdir')
+    subdir = os.path.join("some_dir", "subdir")
 
     with tempfile.TemporaryDirectory() as temp_dir:
         subdir_path = os.path.join(temp_dir, subdir)
@@ -39,17 +39,18 @@ def test_download_html(expected_content, retrieved_content):
             #     assert f.read() == expected_content
 
             expected_saved_html_path = os.path.join(
-                subdir_path, 'ru-hexlet-io-courses.html')
+                subdir_path, "ru-hexlet-io-courses.html"
+            )
             assert os.path.isfile(expected_saved_html_path)
             assert result_path == expected_saved_html_path
 
 
 def test_download_return_value_with_none_path():
     with requests_mock.Mocker() as m:
-        m.get(URL, text='Test content')
+        m.get(URL, text="Test content")
 
         result_path = download(URL)
-        expected_path = 'ru-hexlet-io-courses.html'
+        expected_path = "ru-hexlet-io-courses.html"
         assert os.path.isfile(expected_path)
         assert result_path == expected_path
 
@@ -57,7 +58,7 @@ def test_download_return_value_with_none_path():
 
 
 def test_download_resources(expected_content, retrieved_content):
-    subdir = os.path.join('some_dir', 'subdir')
+    subdir = os.path.join("some_dir", "subdir")
 
     with tempfile.TemporaryDirectory() as temp_dir:
         subdir_path = os.path.join(temp_dir, subdir)
@@ -67,10 +68,12 @@ def test_download_resources(expected_content, retrieved_content):
             m.get(URL, text=retrieved_content)
 
             expected_saved_resources_dir_path = os.path.join(
-                subdir_path, 'ru-hexlet-io-courses_files')
+                subdir_path, "ru-hexlet-io-courses_files"
+            )
             assert os.path.isdir(expected_saved_resources_dir_path)
 
             expected_saved_image_path = os.path.join(
                 expected_saved_resources_dir_path,
-                'ru-hexlet-io-assets-professions-python.png')
+                "ru-hexlet-io-assets-professions-python.png",
+            )
             assert os.path.isfile(expected_saved_image_path)
