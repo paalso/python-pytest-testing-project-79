30f8a23ce3386d1e49f8ae7d7dc96835f7e4209a
Pavel Soroka <40a.pvl@gmail.com>
2023-11-17 16:05:18 +0200

    Implement base logic to load resources

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -5,6 +5,13 @@ from . import url_utils
 
 
 class PageLoader:
+    RESOURCE_TAGS = {
+        'img': 'src',
+        'link': 'href',
+        'video': 'src',
+        'audio': 'src'
+    }
+
     def __init__(self, url, path=None):
         self.url = url
         self.path = path or ''
@@ -32,21 +39,32 @@ class PageLoader:
     def __download_resources(self):
         os.makedirs(self.resources_path)
         for resource in self.__get_page_resources():
-            resource_path = resource.get('src')
+            resource_path = self.__get_resource_path(resource)
             resource_full_url = url_utils.full_url(self.url, resource_path)
-            path_to_save_resource = self.__get_path_to_save_recourse(resource_path)
-            new_resource_basename = os.path.basename(path_to_save_resource)
-            new_resource_path = os.path.join(self.resources_dir, new_resource_basename)
-            self.__download_resource(resource_full_url, path_to_save_resource)
-            resource['src'] = new_resource_path
-            print()
+            path_to_save = self.__get_path_to_save_recourse(resource_path)
+            self.__download_resource(resource_full_url, path_to_save)
+
+            new_resource_basename = os.path.basename(path_to_save)
+            new_resource_path = os.path.join(
+                self.resources_dir, new_resource_basename)
+            resource_link_attr = self.__get_resource_link_attr(resource)
+            resource[resource_link_attr] = new_resource_path
+
+    def __get_resource_link_attr(self, resource):
+        resource_name = resource.name
+        return self.__class__.RESOURCE_TAGS[resource_name]
+
+    def __get_resource_path(self, resource):
+        resource_link_attr = self.__get_resource_link_attr(resource)
+        return resource.get(resource_link_attr)
 
     def __get_raw_page_content(self):
         request = requests.get(self.url)
         return request.text
 
     def __get_page_resources(self):
-        return self.soup.find_all('img')
+        return [resource for tag in self.__class__.RESOURCE_TAGS
+                for resource in self.soup.find_all(tag)]
 
     def __get_path_to_save_recourse(self, resource_url):
         return os.path.join(
