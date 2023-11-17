bc331f0a6d0295331dec7a1be89df65d236c8df5
Pavel Soroka <40a.pvl@gmail.com>
2023-11-17 18:15:28 +0200

    UPD page_loader & tests

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -13,31 +13,31 @@ class PageLoader:
     }
 
     def __init__(self, url, path=None):
+        path = path or ''
         self.url = url
-        self.path = path or ''
         self.path_to_save_page_content = os.path.join(
-            self.path, url_utils.filename_from_url(self.url))
+            path, url_utils.filename_from_url(self.url))
         self.resources_dir = url_utils.dirname_for_web_resources(
             url_utils.filename_from_url(self.url))
         self.resources_path = url_utils.dirname_for_web_resources(
             self.path_to_save_page_content)
-
-        self.original_page_content = self.__get_raw_page_content()
-        self.soup = BeautifulSoup(self.original_page_content, 'html.parser')
-        self.processed_page_content = self.soup.prettify()
-        print()
+        original_page_content = self.__get_raw_page_content()
+        self.soup = BeautifulSoup(original_page_content, 'html.parser')
 
     def download(self):
         self.__download_resources()
+        self.__download_page()
+        return self.path_to_save_page_content
 
+    def __download_page(self):
         processed_page_content = self.soup.prettify()
         with open(self.path_to_save_page_content, 'w') as f:
             f.write(processed_page_content)
 
-        return self.path_to_save_page_content
-
     def __download_resources(self):
-        os.makedirs(self.resources_path)
+        if self.__get_page_resources():
+            os.makedirs(self.resources_path)
+
         for resource in self.__get_page_resources():
             resource_path = self.__get_resource_path(resource)
             resource_full_url = url_utils.full_url(self.url, resource_path)
@@ -71,15 +71,16 @@ class PageLoader:
             self.resources_path,
             url_utils.filename_from_url(resource_url))
 
-    def __download_resource(self, url, save_path):
+    @staticmethod
+    def __download_resource(url, save_path):
         response = requests.get(url)
-        if response.status_code == 200:
+        if response.ok:
             with open(save_path, 'wb') as f:
                 f.write(response.content)
             print(f"resource file '{url}' downloaded successfully"
                   f"and saved to '{save_path}'")
         else:
-            print(f"Failed to download image."
+            print(f"Failed to download resource."
                   f"Status code: {response.status_code}")
 
 
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,78 +2,94 @@ import os
 import requests_mock
 import tempfile
 import pytest
+import shutil
 
 from page_loader.page_loader import download
 
-URL = "https://ru.hexlet.io/courses"
+URL = 'https://ru.hexlet.io/courses'
 
 
 @pytest.fixture
 def expected_content():
-    path = os.path.join("tests", "fixtures", "expected.html")
+    path = os.path.join('tests', 'fixtures', 'expected.html')
     with open(path) as f:
         return f.read()
 
 
 @pytest.fixture
 def retrieved_content():
-    path = os.path.join("tests", "fixtures", "retrieved.html")
+    path = os.path.join('tests', 'fixtures', 'retrieved.html')
     with open(path) as f:
         return f.read()
 
 
-def test_download_html(expected_content, retrieved_content):
-    subdir = os.path.join("some_dir", "subdir")
+@pytest.fixture
+def cleanup_downloaded_files():
+    yield
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        subdir_path = os.path.join(temp_dir, subdir)
-        os.makedirs(subdir_path)
-
-        with requests_mock.Mocker() as m:
-            m.get(URL, text=retrieved_content)
-
-            result_path = download(URL, path=subdir_path)
-
-            assert os.path.isfile(result_path)
-            # with open(result_path, 'r') as f:
-            #     assert f.read() == expected_content
-
-            expected_saved_html_path = os.path.join(
-                subdir_path, "ru-hexlet-io-courses.html"
-            )
-            assert os.path.isfile(expected_saved_html_path)
-            assert result_path == expected_saved_html_path
-
-
-def test_download_return_value_with_none_path():
-    with requests_mock.Mocker() as m:
-        m.get(URL, text="Test content")
-
-        result_path = download(URL)
-        expected_path = "ru-hexlet-io-courses.html"
-        assert os.path.isfile(expected_path)
-        assert result_path == expected_path
-
+    expected_path = 'ru-hexlet-io-courses.html'
+    if os.path.isfile(expected_path):
         os.remove(expected_path)
+    resources_dir = 'ru-hexlet-io-courses_files'
+    if os.path.isdir(resources_dir):
+        shutil.rmtree(resources_dir)
 
 
-def test_download_resources(expected_content, retrieved_content):
-    subdir = os.path.join("some_dir", "subdir")
+@pytest.fixture
+def setup_mocking(retrieved_content):
+    with requests_mock.Mocker() as m:
+        m.get(URL, text=retrieved_content)
+        image_url = 'https://ru.hexlet.io/assets/professions/python.png'
+        image_content = b'Some image content'
+        m.get(image_url, content=image_content)
+        return m
 
+
+@pytest.fixture
+def temp_directory():
     with tempfile.TemporaryDirectory() as temp_dir:
+        subdir = os.path.join('some_dir', 'subdir')
         subdir_path = os.path.join(temp_dir, subdir)
         os.makedirs(subdir_path)
+        return temp_dir, subdir_path
 
-        with requests_mock.Mocker() as m:
-            m.get(URL, text=retrieved_content)
 
-            expected_saved_resources_dir_path = os.path.join(
-                subdir_path, "ru-hexlet-io-courses_files"
-            )
-            assert os.path.isdir(expected_saved_resources_dir_path)
+def test_download_return_value_with_none_path(
+        retrieved_content, cleanup_downloaded_files, setup_mocking):
+    with setup_mocking:
+        result_path = download(URL)
+        assert os.path.isfile(result_path)
+        assert result_path == 'ru-hexlet-io-courses.html'
 
-            expected_saved_image_path = os.path.join(
-                expected_saved_resources_dir_path,
-                "ru-hexlet-io-assets-professions-python.png",
-            )
-            assert os.path.isfile(expected_saved_image_path)
+
+def test_download_html(
+        expected_content, retrieved_content, setup_mocking, temp_directory):
+    temp_dir, subdir_path = temp_directory
+    with setup_mocking:
+        result_path = download(URL, path=subdir_path)
+
+        assert os.path.isfile(result_path)
+        expected_saved_html_path = os.path.join(
+            subdir_path, 'ru-hexlet-io-courses.html'
+        )
+        assert result_path == expected_saved_html_path
+        with open(result_path, 'r') as f:
+            assert f.read() == expected_content
+
+
+def test_download_resources(
+        expected_content, retrieved_content, setup_mocking, temp_directory):
+    temp_dir, subdir_path = temp_directory
+    with setup_mocking:
+        download(URL, path=subdir_path)
+        expected_saved_resources_dir_path = os.path.join(
+            subdir_path, 'ru-hexlet-io-courses_files'
+        )
+        assert os.path.isdir(expected_saved_resources_dir_path)
+
+        expected_saved_image_path = os.path.join(
+            expected_saved_resources_dir_path,
+            'assets-professions-python.png',
+            # 'ru-hexlet-io-assets-professions-python.png',
+        )
+        assert os.path.isfile(expected_saved_image_path)
