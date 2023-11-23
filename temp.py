c43c8b903368835708551d006861e926438845c2
Pavel Soroka <40a.pvl@gmail.com>
2023-11-23 17:47:40 +0200

    Remove files from temp directory

--- temp.py
+++ temp.py
@@ -3,9 +3,9 @@ from page_loader import download
 
 def main():
     url = "https://deti-online.com/skazki/russkie-narodnye-skazki/kolobok/"
-    url = "https://paalso.github.io/a-plus/"
     url = "https://lib.misto.kiev.ua/HAJAM/bio.dhtml"
     url = "https://paalso.github.io/simple_web_page/"
+    url = "https://paalso.github.io/a-plus/"
 
     result = download(url, 'temp')
     print(result)
