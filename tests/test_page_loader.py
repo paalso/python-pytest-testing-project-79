fbeb74743cf2a4229a9c762ae2e2a27fb84bfb78
Pavel Soroka <40a.pvl@gmail.com>
2023-11-24 14:19:11 +0200

    UPD tests.test_page_loader.test_download_images

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -47,10 +47,18 @@ def setup_mocking(retrieved_content):
     with requests_mock.Mocker() as m:
         m.get(URL, text=retrieved_content)
 
-        # Mock the image resource
-        image_url = 'https://ru.hexlet.io/assets/professions/python.png'
-        image_content = b'Some image content'
-        m.get(image_url, content=image_content)
+        # Mock the image resources
+        inner_image1_url = 'https://ru.hexlet.io/assets/professions/python.png'
+        inner_image1_content = b'Some image content'
+        m.get(inner_image1_url, content=inner_image1_content)
+
+        inner_image2_url = 'https://ru.hexlet.io/assets/professions/python.jpg'  # Updated URL
+        inner_image2_content = b'Some updated image content'  # You can update the content if needed
+        m.get(inner_image2_url, content=inner_image2_content)  # Updated mock for the modified image
+
+        external_image_url = 'https://external.net/images/external-image.png'
+        external_image_content = b'Some external image content'
+        m.get(external_image_url, content=external_image_content)
 
         # Mock the script resource
         script_url = 'https://ru.hexlet.io/packs/js/runtime.js'
@@ -97,18 +105,38 @@ def test_download_html(
             assert compare_prettified_htmls(f.read(), expected_content)
 
 
-def test_download_resources(
+def test_download_images(
         expected_content, retrieved_content, setup_mocking, temp_directory):
+
     temp_dir, subdir_path = temp_directory
-    with setup_mocking:
+    with (setup_mocking):
         download(URL, path=subdir_path)
         expected_saved_resources_dir_path = os.path.join(
             subdir_path, 'ru-hexlet-io-courses_files'
         )
+        expected_saved_html_path = os.path.join(
+            subdir_path, 'ru-hexlet-io-courses.html'
+        )
+        with open(expected_saved_html_path, 'r') as file:
+            expected_saved_html_content = file.read()
+
         assert os.path.isdir(expected_saved_resources_dir_path)
 
-        expected_saved_image_path = os.path.join(
+        # Check img links like src="/assets/professions/python.png" download
+        expected_image1_link_in_saved_html = \
+            ('ru-hexlet-io-courses_files/'
+             'ru-hexlet-io-assets-professions-python.png')
+        assert expected_image1_link_in_saved_html in expected_saved_html_content
+
+        expected_saved_image1_path = os.path.join(
             expected_saved_resources_dir_path,
             'ru-hexlet-io-assets-professions-python.png',
         )
-        assert os.path.isfile(expected_saved_image_path)
+        assert os.path.isfile(expected_saved_image1_path)
+
+        # Check that images from external links are not saved
+        external_image_files = [
+            file for file in os.listdir(expected_saved_resources_dir_path)
+            if "external-image.png" in file
+        ]
+        assert not external_image_files
