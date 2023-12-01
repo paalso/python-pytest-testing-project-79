350bfaa19bf15130644c08ba71741e2a144769be
Pavel Soroka <40a.pvl@gmail.com>
2023-12-01 22:33:15 +0200

    UPD page_loader.logger.Logger: refactor and achive good work for page_loader.resource_processor.ResourceProcessor

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -1,54 +1,62 @@
 # page_loader/logger.py
 from datetime import datetime
 import logging
+from dotenv import load_dotenv
 import os
 
 LOGS_DIR = 'logs'
 LOG_FILE_TEMPLATE = 'download_{formatted_page_content_filename}_{date_time}.log'
 TIME_FORMAT = '%Y%m%d_%H%M%S'
-DEBUG_ENV_VARIABLE = 'DEBUG'
 
 
 class Logger:
-    def __init__(self, page_content_filename, log_level=logging.INFO):
-        self.__page_content_filename = page_content_filename
-        self.log_level = log_level
-
-        self.logger = logging.getLogger(__name__)
+    def __init__(self, page_content_filename):
+        self.__log_level = self.__get_level()
+        self.__log_path = self.__get_log_file_path(page_content_filename)
+        self.__logger = logging.getLogger(__name__)
         self.__configure_logger()
-        self.logger.debug(f'log_level: {log_level}')
 
     def debug(self, message):
-        self.logger.debug(message)
+        self.__logger.debug(message)
 
     def info(self, message):
-        self.logger.info(message)
-
-    def is_debug_enabled(self):
-        return os.environ.get(DEBUG_ENV_VARIABLE, '').lower() == 'true'
+        self.__logger.info(message)
 
     def __configure_logger(self):
-        log_file_path = self.__get_log_file_path()
+        self.__logger.setLevel(self.__log_level)
 
-        file_handler = logging.FileHandler(log_file_path)
-        console_handler = logging.StreamHandler()
+        self.__formatter = logging.Formatter(
+            fmt='[%(asctime)s %(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
 
-        if self.log_level < logging.INFO:
-            self.logger.addHandler(file_handler)
-            self.logger.addHandler(console_handler)
+        self.__add_console_handler()
 
-        elif self.log_level == logging.INFO:
-            self.logger.addHandler(console_handler)
+        if self.__log_level < logging.INFO:
+            self.__add_file_handler()
 
-        self.logger.setLevel(self.log_level)
-
-    def __get_log_file_path(self):
+    @staticmethod
+    def __get_log_file_path(filename):
         log_file_dir = LOGS_DIR
-        formatted_page_content_filename = (
-            self.__page_content_filename.rstrip('.html'))
+        formatted_page_content_filename = filename.rstrip('.html')
         date_time = datetime.now().strftime(TIME_FORMAT)
         log_file_name = LOG_FILE_TEMPLATE.format(
             formatted_page_content_filename=formatted_page_content_filename,
             date_time=date_time)
-
         return os.path.join(log_file_dir, log_file_name)
+
+    @staticmethod
+    def __get_level():
+        load_dotenv()
+        log_level = os.environ.get('LOG_LEVEL')
+        return getattr(logging, log_level.upper(), logging.DEBUG)
+
+    def __add_file_handler(self):
+        file_handler = logging.FileHandler(self.__log_path)
+        file_handler.setFormatter(self.__formatter)
+        self.__logger.addHandler(file_handler)
+
+    def __add_console_handler(self):
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(self.__formatter)
+        self.__logger.addHandler(console_handler)
--- page_loader/resource_processor.py
+++ page_loader/resource_processor.py
@@ -26,7 +26,7 @@ class ResourceProcessor:
         self.__resources_path = url_utils.dirname_for_web_resources(
             self.path_to_save_page_content)
 
-        self.logger = Logger(self.__page_content_filename)
+        self.__logger = Logger(self.__page_content_filename)
         self.__log_attributes()
 
     def download_resources(self, resources):
@@ -61,7 +61,7 @@ class ResourceProcessor:
     def __get_resource_url(self, resource):
         resource_link_attr = self.__get_resource_link_attr(resource)
         url = resource.get(resource_link_attr)
-        self.logger.debug(f'Resource URL: {url}')
+        self.__logger.debug(f'Resource URL: {url}')
         return url
 
     # TODO: maybe refactor using page_loader.url_utils.full_url?
@@ -70,26 +70,26 @@ class ResourceProcessor:
             resource_path = f"{resource_path.rstrip('/')}.html"
 
         if url_utils.domain(resource_path):
-            self.logger.debug(f'Resource is an absolute URL: {resource_path}')
+            self.__logger.debug(f'Resource is an absolute URL: {resource_path}')
             return resource_path
 
         if url_utils.is_absolute_path(resource_path):
             full_url = f'{self.__full_domain}{resource_path}'
-            self.logger.debug(f'Full URL for absolute path: {full_url}')
+            self.__logger.debug(f'Full URL for absolute path: {full_url}')
             return full_url
 
         full_url = f'{self.__base_url}{resource_path}'
-        self.logger.debug(f'Full URL for relative path: {full_url}')
+        self.__logger.debug(f'Full URL for relative path: {full_url}')
         return full_url
 
     def __make_resources_dir(self):
         os.makedirs(self.__resources_path)
-        self.logger.debug(
+        self.__logger.debug(
             f'Created resources directory: {self.__resources_path}')
 
     def __remove_resources_dir(self):
         shutil.rmtree(self.__resources_path)
-        self.logger.debug(
+        self.__logger.debug(
             f'Removed empty resources directory: {self.__resources_path}')
 
     # TODO: Refactoring is needed again
@@ -97,13 +97,13 @@ class ResourceProcessor:
     def __get_resource_updated_link(self, resource_full_url):
         base_path_to_save = url_utils.filename_from_full_url(resource_full_url)
         updated_link = os.path.join(self.__resources_dir, base_path_to_save)
-        self.logger.debug(f'Updated resource link: {updated_link}')
+        self.__logger.debug(f'Updated resource link: {updated_link}')
         return updated_link
 
     def __get_path_to_save_resource(self, resource_full_url):
         resource_new_link = self.__get_resource_updated_link(resource_full_url)
         path_to_save = os.path.join(self.__path, resource_new_link)
-        self.logger.debug(f'Path to save resource: {path_to_save}')
+        self.__logger.debug(f'Path to save resource: {path_to_save}')
         return path_to_save
 
     def __get_resource_link_attr(self, resource):
@@ -119,24 +119,24 @@ class ResourceProcessor:
         if response.ok:
             with open(save_path, 'wb') as f:
                 f.write(response.content)
-            self.logger.info(
+            self.__logger.info(
                 f"resource file '{url}' downloaded successfully"
                 f"and saved to '{save_path}'")
         else:
-            self.logger.info(
+            self.__logger.info(
                 f'Failed to download resource.'
                 f'Status code: {response.status_code}')
 
     def __log_attributes(self):
-        self.logger.debug(f'{self.__class__.__name__} initialized')
-        self.logger.debug(f'__path: {self.__path}')
-        self.logger.debug(f'__url: {self.__url}')
-        self.logger.debug(f'__base_url: {self.__base_url}')
-        self.logger.debug(f'__domain: {self.__domain}')
-        self.logger.debug(f'__full_domain: {self.__full_domain}')
-        self.logger.debug(f'__resources_dir: {self.__resources_dir}')
-        self.logger.debug(
+        self.__logger.debug(f'{self.__class__.__name__} initialized')
+        self.__logger.debug(f'__path: {self.__path}')
+        self.__logger.debug(f'__url: {self.__url}')
+        self.__logger.debug(f'__base_url: {self.__base_url}')
+        self.__logger.debug(f'__domain: {self.__domain}')
+        self.__logger.debug(f'__full_domain: {self.__full_domain}')
+        self.__logger.debug(f'__resources_dir: {self.__resources_dir}')
+        self.__logger.debug(
             f'__page_content_filename: {self.__page_content_filename}')
-        self.logger.debug(
+        self.__logger.debug(
             f'path_to_save_page_content: {self.path_to_save_page_content}')
-        self.logger.debug(f'self.resources_path: {self.__resources_path}')
+        self.__logger.debug(f'self.resources_path: {self.__resources_path}')
